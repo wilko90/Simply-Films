a9aeff66eb7a530c3663540b143823fb97041fe2
wilko90 <mark_wilkinson2010@hotmail.co.uk>
2021-06-27 15:04:19 +0000

    feat: add a confirm password filed and login funciton

--- app.py
+++ app.py
@@ -36,6 +36,11 @@ def register():
             flash("Username already exists")
             return redirect(url_for("register"))
 
+        if request.form.get("password") != request.form.get(
+                "confirm-password"):
+            flash("Passwords do not match!")
+            return redirect(url_for("register"))
+
         register = {
             "username": request.form.get("username").lower(),
             "password": generate_password_hash(request.form.get("password"))
@@ -46,6 +51,32 @@ def register():
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
     return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
+            else:
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
+
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
