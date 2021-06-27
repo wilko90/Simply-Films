f9388e575ffe8f311c0a9d78282683f587021476
wilko90 <mark_wilkinson2010@hotmail.co.uk>
2021-06-27 15:52:02 +0000

    feat: add log out function

--- app.py
+++ app.py
@@ -79,6 +79,15 @@ def login():
     return render_template("login.html")
 
 
+@app.route('/logout')
+def logout():
+    """
+    Logs user out.
+    """
+    flash('You have been logged out')
+    session.pop('user')
+    return redirect(url_for("login"))
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
